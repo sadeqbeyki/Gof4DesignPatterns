bac00e0d6a36777b18c6d07e292a015cd1f1a984
mardibak <mardibaki@gmail.com>
2023-01-09 01:24:07 +0330

    Flyweight design pattern implemented

---
+++ DesignPatterns.Flyweight/Alphabet.cs
@@ -0,0 +1,58 @@
+﻿namespace DesignPatterns.Flyweight;
+
+//Step 1
+public abstract class Alphabet
+{
+    protected string character;
+    public void Print(int x, int y, string color)
+    {
+        Console.WriteLine($"x: {x} y: {y} color : {color} {character}");
+    }
+}
+//Step 2
+public class AAlphabet : Alphabet
+{
+    public AAlphabet()
+    {
+        character = "A";
+        Console.WriteLine("Create new instance of A");
+    }
+}
+
+//Step 3
+public class BAlphabet : Alphabet
+{
+    public BAlphabet()
+    {
+        character = "B";
+        Console.WriteLine("Create new instance of B");
+    }
+}
+
+//Step 4 (use simple factory)
+public static class AlphabetFactory
+{
+    static Dictionary<string, Alphabet> dictionary;
+    static AlphabetFactory()
+    {
+        dictionary = new Dictionary<string, Alphabet>();
+    }
+    public static Alphabet GetAlpha(string key)
+    {
+        if (!dictionary.ContainsKey(key))
+        {
+            Alphabet alphabet = null;
+            switch (key)
+            {
+                case "A":
+                    alphabet = new AAlphabet();
+                    break;
+                case "B":
+                    alphabet = new BAlphabet();
+                    break;
+            }
+            dictionary[key] = alphabet;
+        }
+        return dictionary[key];
+    }
+}
---
+++ DesignPatterns.Flyweight/Program.cs
@@ -0,0 +1,11 @@
+﻿//Step 5
+using DesignPatterns.Flyweight;
+
+var a1 = AlphabetFactory.GetAlpha("A");
+var a2 = AlphabetFactory.GetAlpha("A");
+var b1 = AlphabetFactory.GetAlpha("B");
+a1.Print(1, 2, "red");
+a2.Print(2, 2, "blue");
+b1.Print(3, 2, "green");
+
+Console.ReadLine();
