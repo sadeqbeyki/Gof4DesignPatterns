090ec277cc1545b4696d4dd827b12ece1f166a51
mardibak <mardibaki@gmail.com>
2023-01-12 11:44:56 +0330

    Memento Design Patern Implemented

---
+++ DesignPatterns.Memento/Car.cs
@@ -0,0 +1,47 @@
+﻿namespace DesignPatterns.Memento;
+
+//step 2
+public class Car
+{
+    private int _fuel;
+    private int _speed;
+
+    public Car()
+    {
+    }
+
+    public Car(CarState carState)
+    {
+        _fuel = carState.Fuel;
+        _speed = carState.Speed;
+    }
+
+    public void AddFuel(int value)
+    {
+        _fuel += value;
+    }
+
+    public void IncreaseSpeed()
+    {
+        _speed++;
+    }
+
+    public void DecreaseSpeed()
+    {
+        _speed--;
+    }
+
+    public void Kilometer()
+    {
+        _fuel -=6;
+    }
+
+    public CarState GetState()
+    {
+        return new CarState
+        {
+            Fuel = _fuel,
+            Speed = _speed
+        };
+    }
+}
---
+++ DesignPatterns.Memento/CarState.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.Memento;
+
+//step 1
+public class CarState
+{
+    public int Fuel { get; set; }
+    public int Speed { get; set; }
+}
