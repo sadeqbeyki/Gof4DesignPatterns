3ffb8a55f46b2781dd4c6faf740ba3d820706520
mardibak <mardibaki@gmail.com>
2023-01-14 09:19:16 +0330

    proxy design pattern implemented

---
+++ DesignPatterns.Proxy/Person.cs
@@ -0,0 +1,89 @@
+﻿namespace DesignPatterns.Proxy;
+//step 1
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+//step 2
+public interface IPersonRepository
+{
+    List<Person> GetAll();
+    void Add(Person person);
+    Person GetById(int id);
+}
+//step 3
+public class InMemoryPersonRepository : IPersonRepository
+{
+    private readonly List<Person> list = new List<Person>
+    {
+        new Person
+        {
+            Id= 1,
+            FirstName="dorsa",
+            LastName="sahmani"
+        },
+        new Person
+        {
+            Id= 2,
+            FirstName = "Pantea",
+            LastName = "Behzad"
+        }
+    };
+    public void Add(Person person)
+    {
+        person.Id = list.Count + 1;
+        list.Add(person);
+    }
+
+    public List<Person> GetAll()
+    {
+        return list;
+    }
+
+    public Person GetById(int id)
+    {
+        return list.FirstOrDefault(x => x.Id == id);
+    }
+}
+
+//step 4
+public class CachePersonRepository : IPersonRepository
+{
+    private readonly IPersonRepository personRepository;
+
+    public CachePersonRepository(IPersonRepository personRepository)
+    {
+        this.personRepository = personRepository;
+    }
+
+    public void Add(Person person)
+    {
+        personRepository.Add(person);
+    }
+
+    public List<Person> GetAll()
+    {
+        // check cache if not exist
+        if (1 == 1)
+        {
+            var result = personRepository.GetAll();
+            //add result to cache
+        }
+        var cacheResult = new List<Person>(); //Read From Cache
+        return cacheResult;
+    }
+
+    public Person GetById(int id)
+    {
+        // check cache if not exist
+        if (1 == 1)
+        {
+            var result = personRepository.GetAll();
+            //add result to cache
+        }
+        var cacheResult = new Person(); //Read From Cache
+        return cacheResult;
+    }
+}
---
+++ DesignPatterns.Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// step 5
+using DesignPatterns.Proxy;
+//وقتی نمیخوایم از کش استقاده کنیم فالس در غیر این صورت ترووو
+var cacheSetting = false;
+
+IPersonRepository personRepository = 
+    cacheSetting? new CachePersonRepository(new InMemoryPersonRepository()) :
+    new InMemoryPersonRepository();
+
+var searchResult = personRepository.GetAll();
+foreach (var item in searchResult)
+{
+    Console.WriteLine($"{item.Id} {item.FirstName} {item.LastName}");
+}
+Console.WriteLine("Hello, World!");
