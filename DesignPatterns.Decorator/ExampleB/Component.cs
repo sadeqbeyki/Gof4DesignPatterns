63b5b321374a52d0f6f555084dda57259bea5278
sadeqbeyki <mardibaki@gmail.com>
2023-01-23 07:33:36 +0330

    decorator example b implemented

---
+++ DesignPatterns.Decorator/ExampleB/Component.cs
@@ -0,0 +1,8 @@
+﻿// The base Component interface defines operations that can be altered by
+// decorators.
+namespace DesignPatterns.Decorator.ExampleB;
+
+public abstract class Component
+{
+    public abstract string Operation();
+}
---
+++ DesignPatterns.Decorator/ExampleB/ConcreteComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Decorator.ExampleB;
+
+// Concrete Components provide default implementations of the operations.
+// There might be several variations of these classes.
+public class ConcreteComponent : Component
+{
+    public override string Operation()
+    {
+        return "ConcreteComponent";
+    }
+}
+
