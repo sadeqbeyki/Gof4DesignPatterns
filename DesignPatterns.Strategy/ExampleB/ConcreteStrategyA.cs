ddd0ee6e97e99898fccbb230f77a3c60c63bd5eb
sadeqbeyki <mardibaki@gmail.com>
2023-01-31 23:58:22 +0330

    strategy example b implemented

---
+++ DesignPatterns.Strategy/ExampleB/ConcreteStrategyA.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.Strategy.ExampleB;
+
+// Concrete Strategies implement the algorithm while following the base
+// Strategy interface. The interface makes them interchangeable in the
+// Context.
+public class ConcreteStrategyA : IStrategy
+{
+    public object DoAlgorithm(object data)
+    {
+        var list = data as List<string>;
+        list.Sort();
+
+        return list;
+    }
+}
+
---
+++ DesignPatterns.Strategy/ExampleB/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿//step 1
+namespace DesignPatterns.Strategy.ExampleB;
+
+// The Strategy interface declares operations common to all supported
+// versions of some algorithm.
+//
+// The Context uses this interface to call the algorithm defined by Concrete
+// Strategies.
+public interface IStrategy
+{
+    object DoAlgorithm(object data);
+}
+
