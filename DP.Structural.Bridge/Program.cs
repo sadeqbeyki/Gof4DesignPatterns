a82dff189c4e3e33d5f9b4c0cfd82de9a8d6148b
mardibak <mardibaki@gmail.com>
2023-03-02 20:29:54 +0330

    bridge updated A (program file moved)

--- DP.Structural.Bridge/Program.cs
+++ DP.Structural.Bridge/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new();
+
+        Abstraction abstraction;
+        // The client code should be able to work with any pre-configured
+        // abstraction-implementation combination.
+        abstraction = new Abstraction(new ConcreteImplementationA());
+        client.ClientCode(abstraction);
+
+        Console.WriteLine();
+
+        abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+        client.ClientCode(abstraction);
+    }
+}
